a02c7654ad972fe98953f4744fbae4b5f0cb9fe8
7sharp9 <kukulcanenator@googlemail.com>
2013-05-06 10:00:27 +0100

    Updated sln to VS2012 added Copyright

--- src/edge-fs/AssemblyInfo.fs
+++ src/edge-fs/AssemblyInfo.fs
@@ -3,13 +3,13 @@ open System.Reflection
 open System.Runtime.CompilerServices
 
 [<assembly: AssemblyTitle("edge-fs")>]
-[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyDescription("F# compiler for edge.js")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("edge-fs")>]
-[<assembly: AssemblyCopyright("Copyright ©  2013")>]
+[<assembly: AssemblyCopyright("Copyright © 2013 Dave Thomas")>]
 [<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.0.1.0")>]
 //[<assembly: AssemblyDelaySign(false)>]
 //[<assembly: AssemblyKeyFile("")>]
 ()
